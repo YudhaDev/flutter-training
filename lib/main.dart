e4d2605a0ce618bc294042605b23b19dbe41d4d2
YudhaDev <yudha.dev96@gmail.com>
2021-08-27 14:09:29 +0700

    push kedua

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(AplikasiKu());
 
 class AplikasiKu extends StatelessWidget {
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Ini Aplikasi Flutter Pertama")),
-        body: Center(child: Text("Halo Dunia, uhuyy jajjaaj"))
-        )
-    );
+        home: Scaffold(
+            appBar: AppBar(title: Text("Ini Aplikasi Flutter Pertama")),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("ini row pertama"),
+                Text("ini row kedua"),
+                Row(
+                  children: [
+                    Center(
+                        child: Container(
+                          color: Colors.lightBlue,
+                          width: 200,
+                          height: 50,
+                            child: Text(
+                      "Halo Dunia, uhuyy jajjaaj test test",
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, ),
+                    ))),
+                    Container(
+                      color: Colors.redAccent,
+
+                      child: Text("kedua"))
+                  ],
+                )
+              ],
+            )));
   }
 }
 // void main() {
